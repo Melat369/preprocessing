fb40f8a9bfd5beebbf17afdd913906b60c029263
Melat369 <melattesfaye16@gmail.com>
2023-07-21 13:42:23 +0300

    importing the labelEncoder

--- Cata.py
+++ Cata.py
@@ -7,3 +7,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#inserting the dataset
+dataset = pd.read_csv('data.csv')
+X = dataset.iloc[:, :-1]
+Y = dataset.iloc[: 3].values
+
+#taking care of the missing values
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis= 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#encoding the category data
+from sklearn.preprocessing import LabelEncoder
+
