1daeba1e221a708cea8884166cd777ce455a81af
Melat369 <melattesfaye16@gmail.com>
2023-07-21 13:16:20 +0300

    first commit

---
+++ datapre.py
@@ -0,0 +1,23 @@
+#importing the necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+#trying to take care of the missing data sets
+
+from sklearn.preprocessing import Imputers
+
+
+#creating Imputers object
+imputer = Imputers(missing_values = 'NaN', strategy = 'mean', axis = 0)
+
+
+
+#fitting the imputer object
+imputer.fit(x[:, 1:3])
