ab62230898d78c8a7b97bc279f5002d3bc8bc38e
Melat369 <melattesfaye16@gmail.com>
2023-07-21 13:34:26 +0300

    encoding the categorical data into numbers

---
+++ Cata.py
@@ -0,0 +1,9 @@
+#here we will try to encode the categorical variables present in the data.csv
+#into numbers
+
+
+#importing necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
